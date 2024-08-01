ba289703178c3fb603b002afb69bcc82f99d3d5b
Deiner Rodrigues de Souza <155614702+Deiner-S@users.noreply.github.com>
2024-08-01 18:23:40 -0300

    Atualizar o Main.java
    Atualizando Implementação

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,9 @@ public static void main(String[] args) {
     Boolean porta =  true;
     System.out.println("iniciando sistema de cadastro!");
 
-    ClienteMapDAO clienteMapDAO = new ClienteMapDAO();
+    IClienteDAO clienteDAO = new ClienteMapDAO();
 
-    ClienteSetDAO clienteSetDAO = new ClienteSetDAO();
+    
 
 
 
@@ -25,26 +25,26 @@ public static void main(String[] args) {
             String opcao = JOptionPane.showInputDialog(null,"Qual operação gostaria de efetuar ?\n1 - Cadastrar\n2 - EditarCadastro\n3 - ExcluirCadastro\n4 - ExibirTodos\n5 - BuscarCadastro\n\n6 - Sair\n\n Digite o numero da opção ou o nome:", "CRUD", JOptionPane.INFORMATION_MESSAGE);
 
             if (opcao.matches("(?i)Cadastrar|1")) {
-                cadastrar(clienteMapDAO);
-                // cadastrar(clienteSetDAO);
+                cadastrar(clienteDAO);
+               
 
             } else if (opcao.matches("(?i)EditarCadastro|2")) {
-                editarCadastro(clienteMapDAO);
+                editarCadastro(clienteDAO);
                 // editarCadastro(clienteSetDAO);
 
 
             } else if (opcao.matches("(?i)ExcluirCadastro|3")) {
-                excluirCadastro(clienteMapDAO);
-                //excluirCadastro(clienteSetDAO);
+                excluirCadastro(clienteDAO);
+                
 
             } else if (opcao.matches("(?i)ExibirTodos|4")) {
-                listarClientes(clienteMapDAO);
-                //listarClientes(clienteSetDAO);
+                listarClientes(clienteDAO);
+                
 
 
             } else if (opcao.matches("(?i)BuscarCadastro|5")) {
-                buscarCadastro(clienteMapDAO);
-                //buscarCadastro(clienteSetDAO);
+                buscarCadastro(clienteDAO);
+                
 
 
             } else if(opcao.matches("(?i)Sair|6")) {
@@ -62,44 +62,44 @@ public static void main(String[] args) {
 
 }
 
-private static void listarClientes(ClienteMapDAO clienteMapDAO) {
-    JOptionPane.showMessageDialog(null,clienteMapDAO.listarCliente(),"Exibir todos os cadastros", JOptionPane.INFORMATION_MESSAGE);
+private static void listarClientes(IClientDAO clienteDAO) {
+    JOptionPane.showMessageDialog(null,clienteDAO.listarCliente(),"Exibir todos os cadastros", JOptionPane.INFORMATION_MESSAGE);
 }
 
 /**
  *
  */
-private static void buscarCadastro(ClienteMapDAO clienteMapDAO) {
+private static void buscarCadastro(IClienteDAO clienteDAO) {
     String cpf = JOptionPane.showInputDialog(null,"Informe o cpf", JOptionPane.INFORMATION_MESSAGE);
-    JOptionPane.showMessageDialog(null,clienteMapDAO.buscarCliente(cpf),"Buscar cadastro",JOptionPane.INFORMATION_MESSAGE );
+    JOptionPane.showMessageDialog(null,clienteDAO.buscarCliente(cpf),"Buscar cadastro",JOptionPane.INFORMATION_MESSAGE );
 
 }
 /**
  *
  */
-private static void excluirCadastro(ClienteMapDAO clienteMapDAO) {
+private static void excluirCadastro(IClienteDAO clienteDAO) {
     String cpf = JOptionPane.showInputDialog(null,"Informe o cpf", JOptionPane.INFORMATION_MESSAGE);
-    JOptionPane.showMessageDialog(null,clienteMapDAO.excluirCliente(cpf),"Excluir cadastro",JOptionPane.INFORMATION_MESSAGE );
+    JOptionPane.showMessageDialog(null,clienteDAO.excluirCliente(cpf),"Excluir cadastro",JOptionPane.INFORMATION_MESSAGE );
 
 }
 /**
  *
  */
-private static void editarCadastro(ClienteMapDAO clienteMapDAO) {
+private static void editarCadastro(IClienteDAO clienteDAO) {
 
     String cpf = JOptionPane.showInputDialog(null, "Informe o CPF do cadastro que será editado","Editar cadastro", JOptionPane.INFORMATION_MESSAGE);
     String editar = JOptionPane.showInputDialog(null, "Informe o dado que gostaria de editar: nome, cpf, telefone, email, endereco\n\n OBS: somente é possivel alterar 1 por vez","Editar cadastro", JOptionPane.INFORMATION_MESSAGE);
     String novoDado = JOptionPane.showInputDialog(null, "Digite a nova informação de cadastro","Editar cadastro", JOptionPane.INFORMATION_MESSAGE);
-    JOptionPane.showMessageDialog(null,clienteMapDAO.alterarCliente(cpf, editar, novoDado),"Editar cadastro",JOptionPane.INFORMATION_MESSAGE );
+    JOptionPane.showMessageDialog(null,clienteDAO.alterarCliente(cpf, editar, novoDado),"Editar cadastro",JOptionPane.INFORMATION_MESSAGE );
 
 }
 /**
  *
  */
-private static void cadastrar(ClienteMapDAO clienteMapDAO) {
+private static void cadastrar(IClienteDAO clienteDAO) {
     String dadosJuntos = JOptionPane.showInputDialog(null,"Digite os dados do cliente separados por virgula nome,cpf,telefone,email,endereco: ", "Green dinner", JOptionPane.INFORMATION_MESSAGE);
 
-    JOptionPane.showMessageDialog(null,clienteMapDAO.cadastrarCliente(dadosJuntos),"Cadastrar clientes",JOptionPane.INFORMATION_MESSAGE );
+    JOptionPane.showMessageDialog(null,clienteDAO.cadastrarCliente(dadosJuntos),"Cadastrar clientes",JOptionPane.INFORMATION_MESSAGE );
 
 }
 
